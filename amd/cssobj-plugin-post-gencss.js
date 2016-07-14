d64ce9b281c701b511d60f6ab744b23d7bd268d6
1111hui <26634873@qq.com>
2016-07-14 19:25:31 +0800

    FIX: using iife to apply the plugin rule, with option newLine

--- amd/cssobj-plugin-post-gencss.js
+++ amd/cssobj-plugin-post-gencss.js
@@ -8,53 +8,60 @@ define('cssobj_plugin_post_gencss', function () { 'use strict';
       .replace(/(^\s+|\s+$)/g, '')
   }
 
-  function cssobj_plugin_post_gencss (result) {
-    var keys = Object.keys
-    var str = []
-    var walk = function(node) {
-      if (!node) return ''
-      if (node.constructor === Array) return node.map(function (v) {walk(v)})
+  function cssobj_plugin_post_gencss (option) {
 
-      var postArr = []
-      var children = node.children
-      var isGroup = node.type=='group'||node.type=='keyframes'
+    option = option || {newLine: '\n'}
 
-      if(isGroup) {
-        str.push(node.groupText+' {\n')
-      }
+    return function (result) {
+      var newLine = option.newLine
+      var keys = Object.keys
+      var str = []
+      var walk = function(node) {
+        if (!node) return ''
+        if (node.constructor === Array) return node.map(function (v) {walk(v)})
 
-      var prop = node.prop
-      var selText = node.selText
+        var postArr = []
+        var children = node.children
+        var isGroup = node.type=='group'||node.type=='keyframes'
 
-      var cssText = keys(prop).map(function(k) {
-        for(var v, str='', i=prop[k].length; i--; ) {
-          v = prop[k][i]
-          str += k.charAt(0)=='@'
-            ? dashify(k)+' '+v+';\n'
-            : dashify(k)+': '+v+';\n'
+        if(isGroup) {
+          str.push(node.groupText+' {' + newLine)
         }
-        return str
-      }).join('')
 
-      if(keys(prop).length) str.push( selText ? selText + ' {\n'+ cssText +'}\n' : cssText )
+        var prop = node.prop
+        var selText = node.selText
 
-      for(var c in children){
-        if(c==='' || children[c].type=='group') postArr.push(c)
-        else walk(children[c])
+        var cssText = keys(prop).map(function(k) {
+          for(var v, str='', i=prop[k].length; i--; ) {
+            v = prop[k][i]
+            str += k.charAt(0)=='@'
+              ? dashify(k)+' '+v+';' + newLine
+              : dashify(k)+': '+v+';' + newLine
+          }
+          return str
+        }).join('')
+
+        if(keys(prop).length) str.push( selText ? selText + ' {' + newLine + cssText +'}' + newLine : cssText )
+
+        for(var c in children){
+          if(c==='' || children[c].type=='group') postArr.push(c)
+          else walk(children[c])
+        }
+
+        if(isGroup) {
+          str.push('}' + newLine)
+        }
+
+        postArr.map(function(v) {
+          walk(children[v])
+        })
+
       }
-
-      if(isGroup) {
-        str.push('}\n')
-      }
-
-      postArr.map(function(v) {
-        walk(children[v])
-      })
-
+      walk(result.root)
+      result.css = str.join('')
+      return result
     }
-    walk(result.root)
-    result.css = str.join('')
-    return result
+
   }
 
   return cssobj_plugin_post_gencss;
