61d97310feacce69d922c94d229ffaca713244cd
futurist <jamesyang999@gmail.com>
2017-07-26 09:01:13 +0800

    2.0.6 package.json: update browser/module

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,11 @@
 export default {
   entry: 'src/cssobj-plugin-gencss.js',
   moduleName: 'cssobj_plugin_gencss',
-  moduleId: 'cssobj_plugin_gencss',
+  amd: {id: 'cssobj_plugin_gencss'},
   targets: [
     { format: 'iife', dest: 'dist/cssobj-plugin-gencss.iife.js' },
     { format: 'amd',  dest: 'dist/cssobj-plugin-gencss.amd.js'  },
+    { format: 'umd',  dest: 'dist/cssobj-plugin-gencss.amd.js'  },
     { format: 'cjs',  dest: 'dist/cssobj-plugin-gencss.cjs.js'  },
     { format: 'es',   dest: 'dist/cssobj-plugin-gencss.es.js'   }
   ]
